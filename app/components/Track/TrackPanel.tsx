b1622030b6cc89f258a4ea5d4581a0d8d20556a6
stevebarakat <stevebarakat@gmail.com>
2023-09-30 13:39:32 -0400

    trackPanel persisting

--- app/components/Track/TrackPanel.tsx
+++ app/components/Track/TrackPanel.tsx
@@ -1,7 +1,9 @@
-import { type ReactNode } from "react";
-import { MixerMachineContext } from "@/context/MixerMachineContext";
+import { useState, type ReactNode, useEffect } from "react";
+import { MixerMachineContext } from "~/context/MixerMachineContext";
 import { CloseButton } from "@/components/Buttons";
 import { Rnd as FxPanel } from "react-rnd";
+import { useLiveQuery } from "dexie-react-hooks";
+import { db } from "~/db";
 
 type PanelProps = {
   children: ReactNode;
@@ -10,37 +12,65 @@ type PanelProps = {
 
 export default function TrackPanel({ children, trackId }: PanelProps) {
   const { send } = MixerMachineContext.useActorRef();
-  const panelPosition = MixerMachineContext.useSelector((state) => {
-    return state.context.currentTracks[trackId].panelPosition;
-  });
+  const currentTracks = useLiveQuery(
+    async () => await db.currentTracks.toArray()
+  );
 
-  const panelSize = MixerMachineContext.useSelector((state) => {
-    return state.context.currentTracks[trackId].panelSize;
-  });
+  const [panelPosition, setPanelPosition] = useState(
+    currentTracks && currentTracks[trackId].panelPosition
+  );
+
+  const [panelSize, setPanelSize] = useState(
+    currentTracks && currentTracks[trackId].panelSize
+  );
+
+  useEffect(() => {
+    const getCurrentTracks = new Promise((resolve) => resolve(currentTracks));
+    getCurrentTracks.then((value) => {
+      setPanelPosition(value[trackId].panelPosition);
+      setPanelSize(value[trackId].panelSize);
+    });
+  }, [currentTracks, trackId]);
 
   function handleResize(ref: HTMLElement) {
-    send({
-      type: "SET_TRACK_PANEL_SIZE",
+    setPanelSize({
       width: ref.style.width,
-      trackId,
+      height: "auto",
     });
+    if (!currentTracks) return;
+    (async () => {
+      await db.currentTracks
+        .where("path")
+        .equals(currentTracks[trackId].path)
+        .modify({
+          panelSize: {
+            width: ref.style.width,
+            height: "auto",
+          },
+        });
+    })();
   }
 
   function handleMove(d: { x: number; y: number }) {
-    send({
-      type: "SET_TRACK_PANEL_POSITON",
-      x: d.x,
-      y: d.y,
-      trackId,
-    });
+    setPanelPosition({ x: d.x, y: d.y });
+    if (!currentTracks) return;
+    (async () => {
+      await db.currentTracks
+        .where("path")
+        .equals(currentTracks[trackId].path)
+        .modify({ panelPosition: { x: d.x, y: d.y } });
+    })();
   }
 
-  function handleClick() {
-    send({
-      type: "SET_ACTIVE_TRACK_PANELS",
-      trackId,
-    });
+  async function handleClick() {
+    if (!currentTracks) return;
+    await db.currentTracks
+      .where("path")
+      .equals(currentTracks[trackId].path)
+      .modify({ panelActive: !currentTracks[trackId].panelActive });
   }
+
+  console.log("panelSize", panelSize);
 
   return (
     <FxPanel
