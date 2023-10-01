c1f45abe6f386caf41484c98e19fb96951815b8f
stevebarakat <stevebarakat@gmail.com>
2023-10-01 14:18:23 -0400

    seed2

---
+++ prisma/seed2.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
+
+// prisma/seed.ts
+export async function seed2() {
+  const song1 = await db.sourceSong.create({
+    data: {
+      title: "A Day In The Life",
+      slug: "a-day-in-the-life",
+      artist: "The Beatles",
+      year: "1967",
+      studio: "Abby Road",
+      location: "London, England",
+      bpm: 79,
+      start: 0,
+      end: 267,
+    },
+  });
+
+  await db.sourceTrack.create({
+    data: {
+      name: "Bass/Drums",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/aDayInTheLife/bass-drums.mp3",
+    },
+  });
+  await db.sourceTrack.create({
+    data: {
+      name: "Instruments",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/aDayInTheLife/instruments.mp3",
+    },
+  });
+  await db.sourceTrack.create({
+    data: {
+      name: "Orchestra",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/aDayInTheLife/orchestra.mp3",
+    },
+  });
+  await db.sourceTrack.create({
+    data: {
+      name: "Vocals",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/aDayInTheLife/vox.mp3",
+    },
+  });
+}
